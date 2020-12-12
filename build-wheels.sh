150e052021ffc063e773a094b300206eea0a787c
Will Eaton <me@wseaton.com>
2020-12-11 21:04:51 -0500

    update pyproject.toml; start building wheels

---
+++ build-wheels.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -ex
+
+curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable -y
+export PATH="$HOME/.cargo/bin:$PATH"
+
+cd /io
+
+for PYBIN in /opt/python/cp{36,37,38,39}*/bin; do
+    "${PYBIN}/pip" install -U setuptools wheel setuptools-rust
+    "${PYBIN}/python" setup.py bdist_wheel
+done
+
+for whl in dist/*.whl; do
+    auditwheel repair "$whl" -w dist/ --plat linux_x86_64
+done
