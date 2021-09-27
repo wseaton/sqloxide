8f26a3b1f81d28b815bdd912e80f7904af695b81
Will Eaton <wseaton@users.noreply.github.com>
2021-09-27 12:00:09 -0400

    Release PR (#45)
    * update versions of deps
    
    * fix signature syntax
    
    * bump sqlparser-rs version to 0.11.0
    
    * exclude osx/36
    
    * add better error messages
    
    * dedent
    
    * redent properly
    
    * bump version

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
-
 use pythonize::pythonize;
 
 use sqlparser::dialect::*;
@@ -18,23 +17,28 @@ fn string_to_dialect(dialect: &str) -> Box<dyn Dialect> {
         "postgres" => Box::new(PostgreSqlDialect {}),
         "snowflake" => Box::new(SnowflakeDialect {}),
         "sqlite" => Box::new(SQLiteDialect {}),
-        _ => Box::new(GenericDialect {}),
+        _ => {
+            println!("The dialect you chose was not recognized, falling back to 'generic'");
+            Box::new(GenericDialect {})
+        }
     }
 }
 
 /// Function to parse SQL statements from a string. Returns a list with
 /// one item per query statement.
 #[pyfunction]
-#[text_signature = "(sql, dialect)"]
+#[pyo3(text_signature = "(sql, dialect)")]
 fn parse_sql(py: Python, sql: &str, dialect: &str) -> PyResult<PyObject> {
     let chosen_dialect = string_to_dialect(dialect);
     let parse_result = Parser::parse_sql(&*chosen_dialect, sql);
 
     let output = match parse_result {
-        Ok(statements) => pythonize(py, &statements).unwrap(),
+        Ok(statements) => {
+            pythonize(py, &statements).expect("Internal python serialization failed.")
+        }
         Err(_e) => {
             let msg = _e.to_string();
-            return Err(PyValueError::new_err(format!("Parsing failed.\n\t{}", msg)));
+            return Err(PyValueError::new_err(format!("Query parsing failed.\n\t{}", msg)));
         }
     };
 
