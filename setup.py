9f20f89e4d836b8b397ae6e0f6050a4f35bfa7d6
Will Eaton <me@wseaton.com>
2022-03-15 16:49:23 -0400

    Bump version: 0.1.14 → 0.1.15

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ package_data = \
 
 setup_kwargs = {
     'name': 'sqloxide',
-    'version': '0.1.14',
+    'version': '0.1.15',
     'description': 'Python bindings for sqlparser-rs',
     'long_description': open('readme.md').read(),
     'long_description_content_type': 'text/markdown',
