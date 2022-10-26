d815bfa109672e873ce04b39c7b6a279c9e68b05
Will Eaton <wseaton@users.noreply.github.com>
2022-10-25 21:11:05 -0400

    Update build-wheels.sh

--- build-wheels.sh
+++ build-wheels.sh
@@ -6,7 +6,7 @@ export PATH="$HOME/.cargo/bin:$PATH"
 
 cd $GITHUB_WORKSPACE/io
 
-for PYBIN in /opt/python/cp{37,38,39,310}*/bin; do
+for PYBIN in /opt/python/cp{37,38,39,310,311}*/bin; do
     "${PYBIN}/pip" install -U setuptools wheel setuptools-rust
     "${PYBIN}/python" setup.py bdist_wheel
 done
