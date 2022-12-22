1993325c16b2478fba3ab3d0b3505ce0793c55ec
dhaval <dhavalmendapara123@gmail.com>
2022-12-22 22:54:02 +0530

    updated ci.yml

--- build-wheels.sh
+++ build-wheels.sh
@@ -5,6 +5,7 @@ curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable -y
 export PATH="$HOME/.cargo/bin:$PATH"
 
 cd $GITHUB_WORKSPACE/io
+cd io
 
 for PYBIN in /opt/python/cp{37,38,39,310,311}*/bin; do
     "${PYBIN}/pip" install -U setuptools wheel setuptools-rust
